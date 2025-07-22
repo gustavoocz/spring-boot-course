90ceef717128b923f3b2d9ea22cf55e51e477dd6
Gustavo Coelho Serezini <serezinigustavo@gmail.com>
2025-07-22 10:10:14 -0300

    Adding migrations support with Flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'São Paulo - Brasil', 'Ayrton', 'Male', 'Senna'),
+	(2, 'Anchiano - Italy - 1500', 'Leonardo', 'Male', 'da Vincci'),
+	(4, 'Porbandar - India', 'Indira', 'Female', 'GAnchi'),
+	(7, 'kentucky - US', 'muhamad', 'Male', 'ali'),
+	(9, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(10, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(11, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+	(12, 'Smiljan - Croatia', 'Nikola', 'Male', 'Tesla');
