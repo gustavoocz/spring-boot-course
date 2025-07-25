cde986d79774d1acc186f53d2b2a1b1cd44e0753
Gustavo Coelho Serezini <serezinigustavo@gmail.com>
2025-07-25 15:57:35 -0300

    Implementing Content Negociation with JSON, XML AND YAML

--- src/main/java/br/com/gustavoocz/controller/PersonController.java
+++ src/main/java/br/com/gustavoocz/controller/PersonController.java
@@ -16,14 +16,18 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @GetMapping (produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping (produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
     public List<PersonDTO> findAll(){
 
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}",
-            produces = MediaType.APPLICATION_JSON_VALUE
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO findById(@PathVariable("id") Long id){
 
@@ -31,8 +35,12 @@ public class PersonController {
     }
 
     @PostMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO create(@RequestBody PersonDTO person){
 
@@ -40,8 +48,12 @@ public class PersonController {
     }
 
     @PutMapping(
-            consumes = MediaType.APPLICATION_JSON_VALUE,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            consumes = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE}
     )
     public PersonDTO update(@RequestBody PersonDTO person){
 
